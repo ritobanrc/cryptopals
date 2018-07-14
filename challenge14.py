f4faf7bc53f635b36224965669ed910036b4eace
ritoban123 <ritobanrc@gmail.com>
2018-07-13 21:47:07 -0700

    challenge 14

--- challenge14.py
+++ challenge14.py
@@ -5,10 +5,11 @@ from Crypto.Cipher import AES
 import challenge11
 import challenge9
 
-from random import randint
+from random import randint, seed
 
-random_key = challenge11.random_string(AES.block_size)
-to_prepend = challenge11.random_string(randint(1, 32))
+from math import floor
+
+
 
 
 def encrypt_oracle(plaintext):
@@ -40,7 +41,7 @@ def decrypt_block(oracle, block_size, prevs, offset):
     # we need to find the appended text 1 block at a time. Let's start with the first block.
     known = bytearray(bytes(1) * block_size)
     for i in range(0, block_size):
-        short = (b'=' * (block_size - i - 1))
+        short = b'?' * offset[1] + (b'=' * (block_size - i - 1))
         short_encrypted = oracle(bytes(short))
         # print('i: ', i, ' ', binascii.hexlify(short_encrypted[:(len(prevs)+block_size)]), '\n', short)
         for x in range(255):
@@ -52,42 +53,63 @@ def decrypt_block(oracle, block_size, prevs, offset):
             ciphertext = oracle(bytes(plaintext))
             # print(plaintext, ': ', binascii.hexlify(ciphertext[:(len(prevs)+block_size)]))
 
-            if ciphertext[offset + len(prevs):(offset + len(prevs) + block_size)] == \
-                    short_encrypted[offset + len(prevs):(offset + len(prevs) + block_size)]:
+            if ciphertext[offset[0] + len(prevs):(offset[0] + len(prevs) + block_size)] == \
+                    short_encrypted[offset[0] + len(prevs):(offset[0] + len(prevs) + block_size)]:
                 known[i] = x
                 continue
     return bytes(known)
 
 
 def guess_offset(oracle):
-    empty_input = oracle(b'')
-    one_char = oracle(b'A')
-    offset_block_start = [i for i in range(len(empty_input)) if empty_input[i] == one_char[i]]
-    # if they were equal for less than a contiguous block, ignore. it's a conincidence.
-    # find the last index whose is separated by 1 from the previous (i.e. 13, 14, [15], 94)
-    # this+1 is the rounded down length of the prepended text. => l_min
-    print(offset_block_start)
+    ciphertexts = []
     for i in range(0, AES.block_size+1):
-        ciphertext = binascii.b2a_hex(oracle(b'A'*i))
-        print([ciphertext[i:i + 32] for i in range(0, len(ciphertext), 32)])
-    return 16
+        ciphertext = oracle(b'A'*i)
+        blocks = [ciphertext[i:i+AES.block_size] for i in range(floor(len(ciphertext)/AES.block_size))]
+        ciphertexts.append(blocks)
+    offset = 0
+
+    for i in range(0, len(ciphertexts[0])):
+        if ciphertexts[0][i] != ciphertexts[1][i]:
+            offset += AES.block_size * i
+            first_non_eq_block = i
+            break
+    q = 16
+    for i in range(0, len(ciphertexts) - 1):
+        if ciphertexts[i][first_non_eq_block] == ciphertexts[i+1][first_non_eq_block]:
+            q = i
+            break
+    offset += (AES.block_size - q)
+    return offset, q
 
 
 def main():
-    # plaintext = ''.join(open('challenge12_data.txt').readlines()).encode()
+    global random_key
+    global to_prepend
+    random_key = challenge11.random_string(AES.block_size)
+    to_prepend = challenge11.random_string(randint(1, 32))
+
     guessed_block_size = guess_block_size(encrypt_oracle)
-    guessed_ecb = challenge11.guess_ecb(encrypt_oracle(b'YELLOW SUBMARINEYELLOW SUBMARINE'))
     guessed_offset = guess_offset(encrypt_oracle)
-    print('Block size: ', guessed_block_size)
-    print('ECB: ', guessed_ecb)
-    print('Offset: ', guessed_offset)
+    guessed_ecb = challenge11.guess_ecb(encrypt_oracle(b'?' * guessed_offset[1] + b'YELLOW SUBMARINEYELLOW SUBMARINE'))
+    # print('Block size: ', guessed_block_size)
+    # print('ECB: ', guessed_ecb)
+    # print('Offset: ', guessed_offset)
     prevs = bytearray()
     block_n = 0
     for i in range(24):
         prevs += decrypt_block(encrypt_oracle, guessed_block_size, prevs, guessed_offset)
         block_n += 1
-    print(prevs)
+    #print(prevs)
+    correct = bytearray(b'Three Rings for the Elven-kings under the sky,\r\nSeven for the Dwarf-lords in their halls'
+                        b' of stone,\r\nNine for Mortal Men doomed to die,\r\nOne for the Dark Lord on his dark throne'
+                        b'\r\nIn the Land of Mordor where the Shadows lie.\r\nOne Ring to rule them all, One Ring to'
+                        b' find them,\r\nOne Ring to bring them all, and in the darkness bind them,\r\nIn the Land of '
+                        b'Mordor where the Shadows lie.\x01\x00\x00\x00\x00')
+    if prevs != correct:
+        print('Error at: ', guessed_offset)
 
 
 if __name__ == '__main__':
-    main()
+    seed(0)
+    for i in range(100):
+        main()
