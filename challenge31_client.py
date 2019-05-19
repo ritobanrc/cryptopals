196921dbb6e4fc6823b0cea6998296b2403a2547
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2019-05-19 11:46:12 -0700

    Finished challenge 31 w/ server and client

---
+++ challenge31_client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import requests
+import binascii
+
+URL = '127.0.0.1'
+PORT = 5000
+
+def make_request(mac):
+    payload = {'file': 'challenge31_data.txt', 'signature': binascii.hexlify(mac)}
+    response = requests.get(f'http://{URL}:{PORT}', payload)
+    if response.status_code == 200:
+        print(f'Success. MAC: {binascii.hexlify(mac)}')
+        return True, 0
+    return False, response.elapsed.total_seconds()
+
+def main():
+    mac = bytearray()
+    for i in range(64):
+        max_time = 0
+        for c in range(0, 255):
+            success, elapsed = make_request(mac + bytes([c]))
+            if success:
+                break
+            if elapsed > max_time:
+                max_time = elapsed
+                char = c
+        mac.append(char)
+        print(max_time, hex(char))
+        if success:
+            print(binascii.hexlify(mac))
+            break
+
+
+if __name__ == '__main__':
+    main()
+
+
--- challenge31_server.py
+++ challenge31_server.py
@@ -3,8 +3,8 @@ import os
 import random
 import binascii
 import time
-from flask import request, abort
-from flask_api import FlaskAPI, status
+from flask import request
+from flask_api import FlaskAPI
 from my_hmac import hmac_sha1
 
 APP = FlaskAPI(__name__)
@@ -12,26 +12,26 @@ APP = FlaskAPI(__name__)
 with open('/usr/share/dict/words') as f:
     key = bytes(random.choice(f.readlines()), encoding='utf8')[:-1]
 
-print(key)
-
 @APP.route('/')
 def index():
     filename = request.args.get('file', '')
     submitted_mac = request.args.get('signature', '')
-    print(filename, os.path.exists(filename))
     if filename == '' or submitted_mac == '':
-        return ''
+        return '', 400
     if not os.path.exists(filename):
-        return ''
+        return '', 400
     with open(filename, 'rb') as f:
         data = f.read()
     actual_mac = hmac_sha1(key, data)
+    print(binascii.hexlify(actual_mac))
     submitted_mac = binascii.unhexlify(submitted_mac)
     if insecure_compare(actual_mac, submitted_mac):
         return f'<h1>{filename}</h1> {str(data)}', 200
-    abort(500)
+    return '', 500
 
 def insecure_compare(buf1, buf2):
+    if len(buf1) > len(buf2):
+        buf2 += b'\x00' * (len(buf1) - len(buf2))
     for b1, b2 in zip(buf1, buf2):
         if not b1 == b2:
             return False
